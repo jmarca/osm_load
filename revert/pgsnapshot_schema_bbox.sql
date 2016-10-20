858d5b1b53d15c3829409d8831186411627f8eef
James Marca <jmarca@translab.its.uci.edu>
2016-10-20 12:11:21 -0700

    fix revert of bbox

--- revert/pgsnapshot_schema_bbox.sql
+++ revert/pgsnapshot_schema_bbox.sql
@@ -2,6 +2,15 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+SET search_path to osm,public;
+
+-- not sure what to do for a rollback here
+-- seems if it fails, it is failed.
+
+drop aggregate osm.first(anyelement);
+drop function osm.first_agg(anyelement,anyelement);
+drop index idx_ways_bbox;
+alter table ways drop column bbox;
+
 
 COMMIT;
