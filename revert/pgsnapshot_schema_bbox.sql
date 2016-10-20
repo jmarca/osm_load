61b48896aefbf76c7213aa84558c44683b3608a7
James Marca <jmarca@translab.its.uci.edu>
2016-10-20 12:12:53 -0700

    remove comment that is not relevant

--- revert/pgsnapshot_schema_bbox.sql
+++ revert/pgsnapshot_schema_bbox.sql
@@ -4,9 +4,6 @@ BEGIN;
 
 SET search_path to osm,public;
 
--- not sure what to do for a rollback here
--- seems if it fails, it is failed.
-
 drop aggregate osm.first(anyelement);
 drop function osm.first_agg(anyelement,anyelement);
 drop index idx_ways_bbox;
